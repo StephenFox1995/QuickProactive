07d56b97cc40146e4a1a9894df3a8e61c9976769
Stephen Fox <stephenfox995@hotmail.com>
2017-01-29 17:49:40 +0000

    removed rate-limit and caching from travel class, as it is very unlikely that the same coords will be used over and over.

--- proactive/travel/travel.py
+++ proactive/travel/travel.py
@@ -1,37 +1,35 @@
-from datetime import datetime
 import googlemaps
 from .gmapsresponse import GmapsResponse
 
 
 class Travel(object):
-  def __init__(self, gmapsKey, rateLimit=20):
+  class GmapsFactory(object):
+    @staticmethod
+    def newGmapsClient(key):
+      return googlemaps.Client(key=key)
+
+
+  def __init__(self, gmapsKey):
     """
       Initialises a new instance of the Travel class
       which can be used for finding travel information
       from one place to another using the Google Maps API.
 
       @param gmapsKey:(string) The Google Maps API key.
-
-      @param rateLimit:(int) The time to wait until a new request is sent
-      if the same origin and destination are used. If different locations are
-      used than the previous invocation of #find() then rate limit will not take effect.
     """
     self._gmapsKey = gmapsKey
-    self._gmaps = googlemaps.Client(key=self._gmapsKey)
-    self._rateLimit = rateLimit
-    self._lastOrig = None
-    self._lastDest = None
-    self._mostRecentRequestTime = None
-    self._mostRecentResponse = None
+    self._gmapsClient = self.GmapsFactory.newGmapsClient(key=self._gmapsKey)
 
 
   def find(self, orig, dest, metric, mode="walking", measure="value"):
     """
       Method to find some travel information about two different locations.
 
-      @param orig See googlemaps.distance_matrix for correct type.
+      @param orig:(See googlemaps.distance_matrix for correct type)
+        The coordinates of the origin location.
 
-      @param dest See googlemaps.distance_matrix for correct type.
+      @param dest:(See googlemaps.distance_matrix for correct type)
+        The coordinates of the destination location.
 
       @param metric:(string) Specify the metric in relation to the two
         locations, currently supported is 'distance', 'duration'.
@@ -41,23 +39,6 @@ class Travel(object):
       @param measure:(string) Whether to return a text based measure or a correct
         measure according to the value. E.g text=4.32km, value=4.25
     """
-    shouldFetch = False
-    # New location to find?
-    if self._lastOrig != orig and self._lastDest != dest:
-      shouldFetch = True
-
-    self._lastOrig = orig
-    self._lastDest = dest
-
-    if self._mostRecentRequestTime != None:
-      # Check if a new request should be sent, based on the rateLimit value.
-      if (datetime.now() - self._mostRecentRequestTime).total_seconds() >= self._rateLimit:
-        shouldFetch = True
-
-    if shouldFetch:
-      # Send request.
-      self._mostRecentResponse = GmapsResponse(self._gmaps.distance_matrix(orig, dest, mode=mode))
-      # Record the request time
-      self._mostRecentRequestTime = datetime.now()
-
-    return self._mostRecentResponse.matrixInfo(metric, measure)
+    # Send request.
+    response = GmapsResponse(self._gmapsClient.distance_matrix(orig, dest, mode=mode))
+    return response.matrixInfo(metric, measure)
