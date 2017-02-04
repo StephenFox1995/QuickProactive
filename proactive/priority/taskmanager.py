5e534013bc278eac6fb32d0b7f2f59aa8e72419b
Stephen Fox <stephenfox995@hotmail.com>
2017-02-04 17:48:34 +0000

    Added ConflictSet class to perform queries about conflict sets.

--- proactive/priority/taskmanager.py
+++ proactive/priority/taskmanager.py
@@ -1,20 +1,44 @@
 from intervaltree import IntervalTree
 
+class ConflictSet(object):
+  def __init__(self, conflicts):
+    self._conflicts = conflicts
+
+  def allLessThanOrEqual(self, value):
+    conflicts = []
+    for conflict in self._conflicts:
+      if len(conflict) <= value:
+        conflicts.append(conflict)
+    return conflicts
+
+  def allGreaterThan(self, value):
+    conflicts = []
+    for conflict in self._conflicts:
+      if len(conflict) > value:
+        conflicts.append(conflict)
+    return conflicts
+
+
+
 class TaskManager(object):
+
   def __init__(self, period):
     self.__tasks = []
     self.__intervalTree = IntervalTree()
     self.__start = period[0]
     self.__end = period[1]
 
+
   def addTask(self, task):
     self.__tasks.append(task)
     self.__addTaskToTree(task)
+
 
   def addTasks(self, tasks):
     for task in tasks:
       self.__tasks.append(task)
       self.__addTaskToTree(task)
+
 
   def __addTaskToTree(self, task):
     self.__intervalTree.addi(
@@ -23,22 +47,33 @@ class TaskManager(object):
       data=task["id"]
     )
 
+  def findConflicts(self):
+    """
+      Finds all the conflicts within the tasks set.
+      A conflict being, any two or more tasks that need
+      to be proccessed at some point simultaneously.
+      In terms of an interval tree, the two task times 'overlap'.
 
-  def findConflicts(self, threshold):
+      This method finds all the conflicts of the current task set
+      held by this class.
+    """
+
     begin = self.__intervalTree.begin()
     end = self.__intervalTree.end()
-    underThresholdConflicts = []
-    overThresholdConflict = []
-
+    conflicts = []
     intervals = sorted(self.__intervalTree[begin:end])
     for interval in intervals:
       _intervals = self.__intervalTree[interval.begin:interval.end]
       if len(_intervals) > 1: # theres a conflict
-        if len(_intervals) <= threshold:
-          if _intervals not in underThresholdConflicts:
-            underThresholdConflicts.append(_intervals)
-        else:
-          if _intervals not in overThresholdConflict:
-            overThresholdConflict.append(_intervals)
-    return (underThresholdConflicts, overThresholdConflict)
+        if _intervals not in conflicts:
+          conflicts.append(_intervals)
+    return ConflictSet(conflicts)
+
+
+  def __flattenIntervalSet(self, intervalSet):
+    intervals = []
+    for x in intervalSet:
+      for y in x:
+        intervals.append(y)
+    return intervals
 
--- tests/test_taskmanager.py
+++ tests/test_taskmanager.py
@@ -3,22 +3,18 @@ from intervaltree import IntervalTree
 from proactive.priority.taskmanager import TaskManager
 
 class TestTaskManager(TestCase):
-  def setUp(self):
-   self._tasks = [
-     {"id": "t1", "release": 9.30, "deadline": 10.00, "status": "unassigned"},
-     {"id": "t2", "release": 9.40, "deadline": 11.00, "status": "unassigned"},
-     {"id": "t3", "release": 11.30, "deadline": 12.00, "status": "unassigned"},
-     {"id": "t4", "release": 11.35, "deadline": 12.00, "status": "unassigned"},
-     {"id": "t5", "release": 11.50, "deadline": 12.30, "status": "unassigned"},
-     {"id": "t6", "release": 14.50, "deadline": 15.30, "status": "unassigned"},
-  ]
-
 
   def test_numberOfConflicts(self):
+    tasks = [
+      {"id": "t1", "release": 9.30, "deadline": 10.00, "status": "unassigned"},
+      {"id": "t2", "release": 9.40, "deadline": 11.00, "status": "unassigned"},
+      {"id": "t3", "release": 11.30, "deadline": 12.00, "status": "unassigned"},
+      {"id": "t4", "release": 11.35, "deadline": 12.00, "status": "unassigned"},
+      {"id": "t5", "release": 11.50, "deadline": 12.30, "status": "unassigned"}
+    ]
     taskManager = TaskManager(period=(8, 9))
-    taskManager.addTasks(self._tasks)
-    (underThreshold, overThreshold) = taskManager.findConflicts(threshold=2)
-    print(underThreshold)
-    print(overThreshold)
-    self.assertEqual(len(underThreshold), 1)
-    self.assertEqual(len(overThreshold), 1)
+    taskManager.addTasks(tasks)
+    conflicts = taskManager.findConflicts().allGreaterThan(2)
+    self.assertEqual(len(conflicts), 1)
+
+
